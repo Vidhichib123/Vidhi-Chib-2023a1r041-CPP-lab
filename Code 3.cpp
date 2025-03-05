374a868f7cf6c03dd2806e229fb297c267d6c95f
Vidhichib123 <147181290+Vidhichib123@users.noreply.github.com>
2025-03-05 11:10:15 +0530

    Create Code 3.cpp

---
+++ Code 3.cpp
@@ -0,0 +1,21 @@
+/* program to demonstrate the working of function with no return type and no parameter 
+e.g :oop features (class,object, member variable, member functions,access specifiers,)
+*/ 
+# include <iostream>
+using namespace std;
+class Program
+{
+public:void fun()
+{
+    cout<<"Hi";
+}
+   
+};
+
+
+int main()
+{
+   Program obj;
+   obj.fun();
+   return 0;
+}
